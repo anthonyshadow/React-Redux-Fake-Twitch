24fabfd3e7604023d1426bc01d430da2c1aa546c
anthonyshadow <anthony.shadowitz@gmail.com>
2021-07-31 15:41:28 -0400

    add google oauth sign in, add in redux

--- src/components/App.js
+++ src/components/App.js
@@ -5,13 +5,13 @@ import StreamDelete from "../components/streams/StreamDelete";
 import StreamEdit from "../components/streams/StreamEdit";
 import StreamList from "../components/streams/StreamList";
 import StreamShow from "../components/streams/StreamShow";
-// import Header from '../components/Header';
+import Header from '../components/Header';
 
 const App = () => {
   return (
     <div className="ui container">
       <BrowserRouter>
-        {/* <Header /> */}
+        <Header />
         <div>
           <Route path="/" exact component={StreamList} />
           <Route path="/streams/new" exact component={StreamCreate} />
